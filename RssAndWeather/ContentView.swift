9900794b6d3123383f1e1f27cf25e2375a6e9a0d
akasoul <antonvoloshuk@gmail.com>
2021-07-12 13:27:28 +0300

    Initial Commit

---
+++ RssAndWeather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RssAndWeather
+//
+//  Created by Anton Voloshuk on 12.07.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RssAndWeather/RssAndWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  RssAndWeatherApp.swift
+//  RssAndWeather
+//
+//  Created by Anton Voloshuk on 12.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct RssAndWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
